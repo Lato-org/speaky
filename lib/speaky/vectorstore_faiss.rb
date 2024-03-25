b0a86f9a3ff84f2466a74e9d4b0e8bb15491e054
Gregorio Galante <me@gregoriogalante.com>
2024-03-25 22:03:07 +0100

    Temporary disabled faiss

--- lib/speaky/vectorstore_faiss.rb
+++ lib/speaky/vectorstore_faiss.rb
@@ -1,51 +1,51 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'faiss'
+# require 'faiss'
 
-module Speaky
-  class VectorstoreFaiss < VectorstoreBase
-    def initialize(config)
-      @config = config
+# module Speaky
+#   class VectorstoreFaiss < VectorstoreBase
+#     def initialize(config)
+#       @config = config
 
-      # check if the index path is set
-      raise ArgumentError, 'index_path is required' unless @config[:index_path]
+#       # check if the index path is set
+#       raise ArgumentError, 'index_path is required' unless @config[:index_path]
 
-      # load index from index_path if exists
-      if File.exist?(@config[:index_path])
-        @index = Faiss::Index.load(@config[:index_path])
-      else
-        # create a new index
-        index = Faiss::IndexFlatL2.new(1536)
-        @index = Faiss::IndexIDMap.new(index)
-        @index.save(@config[:index_path])
-      end
-    end
+#       # load index from index_path if exists
+#       if File.exist?(@config[:index_path])
+#         @index = Faiss::Index.load(@config[:index_path])
+#       else
+#         # create a new index
+#         index = Faiss::IndexFlatL2.new(1536)
+#         @index = Faiss::IndexIDMap.new(index)
+#         @index.save(@config[:index_path])
+#       end
+#     end
 
-    def add(id, data)
-      embeddings = Speaky.llm.embed(data)
+#     def add(id, data)
+#       embeddings = Speaky.llm.embed(data)
 
-      @index.add_with_ids([embeddings], [string_id_to_unique_int_id(id)])
+#       @index.add_with_ids([embeddings], [string_id_to_unique_int_id(id)])
 
-      true
-    end
+#       true
+#     end
 
-    def remove(id)
-      # remove is not supported by Faiss
-      true
-    end
+#     def remove(id)
+#       # remove is not supported by Faiss
+#       true
+#     end
 
-    def query(question)
-      # TODO: implement query
+#     def query(question)
+#       # TODO: implement query
 
-      []
-    end
+#       []
+#     end
 
-    private
+#     private
 
-    # This method is used to convert a string ID to a unique integer ID
-    # that can be used by the Qdrant API.
-    def string_id_to_unique_int_id(string_id)
-      string_id.to_s.hash.abs
-    end
-  end
-end
+#     # This method is used to convert a string ID to a unique integer ID
+#     # that can be used by the Qdrant API.
+#     def string_id_to_unique_int_id(string_id)
+#       string_id.to_s.hash.abs
+#     end
+#   end
+# end
--- spec/speaky/vectorstore_faiss_spec.rb
+++ spec/speaky/vectorstore_faiss_spec.rb
@@ -1,74 +1,74 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require "spec_helper"
+# require "spec_helper"
 
-RSpec.describe Speaky::VectorstoreFaiss do
-  it "should be initialized with a configuration" do
-    next unless FAISS_CONFIGURED
+# RSpec.describe Speaky::VectorstoreFaiss do
+#   it "should be initialized with a configuration" do
+#     next unless FAISS_CONFIGURED
 
-    vectorstore = Speaky::VectorstoreFaiss.new({
-      index_path: ENV["FAISS_INDEX_PATH"]
-    })
+#     vectorstore = Speaky::VectorstoreFaiss.new({
+#       index_path: ENV["FAISS_INDEX_PATH"]
+#     })
 
-    expect(vectorstore.config).to eq({
-      index_path: ENV["FAISS_INDEX_PATH"]
-    })
-  end
+#     expect(vectorstore.config).to eq({
+#       index_path: ENV["FAISS_INDEX_PATH"]
+#     })
+#   end
 
-  it "should raise an error if index_path is not set" do
-    next unless FAISS_CONFIGURED
+#   it "should raise an error if index_path is not set" do
+#     next unless FAISS_CONFIGURED
 
-    expect {
-      Speaky::VectorstoreFaiss.new({})
-    }.to raise_error(ArgumentError)
-  end
+#     expect {
+#       Speaky::VectorstoreFaiss.new({})
+#     }.to raise_error(ArgumentError)
+#   end
 
-  it "should add a vector to the index" do
-    next unless FAISS_CONFIGURED && OPENAI_CONFIGURED
+#   it "should add a vector to the index" do
+#     next unless FAISS_CONFIGURED && OPENAI_CONFIGURED
 
-    # HACK: force speaky @llm to be initialized with OpenAI config
-    Speaky.instance_variable_set(:@llm, Speaky::LlmOpenai.new({
-      access_token: ENV["OPENAI_ACCESS_TOKEN"]
-    }))
+#     # HACK: force speaky @llm to be initialized with OpenAI config
+#     Speaky.instance_variable_set(:@llm, Speaky::LlmOpenai.new({
+#       access_token: ENV["OPENAI_ACCESS_TOKEN"]
+#     }))
 
-    vectorstore = Speaky::VectorstoreFaiss.new({
-      index_path: ENV["FAISS_INDEX_PATH"]
-    })
+#     vectorstore = Speaky::VectorstoreFaiss.new({
+#       index_path: ENV["FAISS_INDEX_PATH"]
+#     })
 
-    vectorstore.add("test", "Hello, world!")
+#     vectorstore.add("test", "Hello, world!")
 
-    # HACK: reset speaky @llm
-    Speaky.instance_variable_set(:@llm, nil)
-  end
+#     # HACK: reset speaky @llm
+#     Speaky.instance_variable_set(:@llm, nil)
+#   end
 
-  it "should remove a vector from the index" do
-    next unless FAISS_CONFIGURED
+#   it "should remove a vector from the index" do
+#     next unless FAISS_CONFIGURED
 
-    vectorstore = Speaky::VectorstoreFaiss.new({
-      index_path: ENV["FAISS_INDEX_PATH"]
-    })
+#     vectorstore = Speaky::VectorstoreFaiss.new({
+#       index_path: ENV["FAISS_INDEX_PATH"]
+#     })
 
-    expect(vectorstore.remove("test")).to eq(true)
-  end
+#     expect(vectorstore.remove("test")).to eq(true)
+#   end
 
-  it "should query the index" do
-    next unless FAISS_CONFIGURED && OPENAI_CONFIGURED
+#   it "should query the index" do
+#     next unless FAISS_CONFIGURED && OPENAI_CONFIGURED
 
-    # HACK: force speaky @llm to be initialized with OpenAI config
-    Speaky.instance_variable_set(:@llm, Speaky::LlmOpenai.new({
-      access_token: ENV["OPENAI_ACCESS_TOKEN"]
-    }))
+#     # HACK: force speaky @llm to be initialized with OpenAI config
+#     Speaky.instance_variable_set(:@llm, Speaky::LlmOpenai.new({
+#       access_token: ENV["OPENAI_ACCESS_TOKEN"]
+#     }))
 
-    vectorstore = Speaky::VectorstoreFaiss.new({
-      index_path: ENV["FAISS_INDEX_PATH"]
-    })
+#     vectorstore = Speaky::VectorstoreFaiss.new({
+#       index_path: ENV["FAISS_INDEX_PATH"]
+#     })
 
-    result = vectorstore.query("Hello what?")
-    expect(result).to be_an(Array)
+#     result = vectorstore.query("Hello what?")
+#     expect(result).to be_an(Array)
 
-    puts result
+#     puts result
 
-    # HACK: reset speaky @llm
-    Speaky.instance_variable_set(:@llm, nil)
-  end
-end
+#     # HACK: reset speaky @llm
+#     Speaky.instance_variable_set(:@llm, nil)
+#   end
+# end
